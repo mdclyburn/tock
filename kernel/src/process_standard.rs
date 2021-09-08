4b57e73536675674fdf54be84e1b3573cb85c4ac
Marshall Clyburn <mardev2013@gmail.com>
2021-09-08 09:43:40 -0400

    Count custom grants in bulk

--- kernel/src/process_standard.rs
+++ kernel/src/process_standard.rs
@@ -1680,6 +1680,7 @@ impl<C: 'static + Chip> ProcessStandard<'_, C> {
         memstat_set!(crate::hil::memstat::CounterId::GrantPointerTable(pid), grant_ptrs_offset);
         memstat_set!(crate::hil::memstat::CounterId::PCB(pid), Self::PROCESS_STRUCT_OFFSET);
         memstat_set!(crate::hil::memstat::CounterId::UpcallQueue(pid), Self::CALLBACKS_OFFSET);
+        memstat_set!(crate::hil::memstat::CounterId::CustomGrant(pid), 0);
 
         process.process_id.set(pid);
         process.kernel = kernel;
@@ -2020,10 +2021,15 @@ impl<C: 'static + Chip> ProcessStandard<'_, C> {
                     let new_kbrk = new_break as usize;
                     memstat_mod!(crate::hil::memstat::CounterId::AllGrantStructures(self.processid()),
                                  (old_kbrk - new_kbrk) as isize);
-                    // Not tracking custom grants.
+
+                    // If a grant number was supplied by the caller, then this is a standard grant.
+                    // Otherwise, it is a custom grant which would be a bit harder to track down a source for.
                     if let Some(grant_no) = grant_no {
                         memstat_set!(crate::hil::memstat::CounterId::Grant(self.processid(), grant_no),
                                      (old_kbrk - new_kbrk));
+                    } else {
+                        memstat_mod!(crate::hil::memstat::CounterId::CustomGrant(self.processid()),
+                                     (old_kbrk - new_kbrk) as isize);
                     }
                 }
                 self.kernel_memory_break.set(new_break);
