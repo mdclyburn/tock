6e0686721b7ef9196dd810cf5c8e18d8059d6b50
Marshall Clyburn <mardev2013@gmail.com>
2022-02-24 10:26:37 -0500

    Export use_instance()

--- perf/src/lib.rs
+++ perf/src/lib.rs
@@ -3,7 +3,8 @@
 mod counter;
 mod proto;
 
-pub use counter::PerformanceCounter;
 pub use counter::INSTANCE;
+pub use counter::PerformanceCounter;
+pub use counter::use_instance;
 
 pub use proto::TX_BUFFER_BYTE_LEN;
