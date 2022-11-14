74ae2d14d896c02bb8852f629ab36d5b60329df0
Marshall Clyburn <mardev2013@gmail.com>
2022-11-14 14:55:24 -0500

    Put ADC channel state in accounting

--- boards/raspberry_pi_pico/src/energy.rs
+++ boards/raspberry_pi_pico/src/energy.rs
@@ -1,31 +1,21 @@
 /*! Energy accounting for the Raspberry Pi Pico.
  */
 
+use core::cell::Cell;
+
 use kernel::energy::DriverEnergyAccounting;
 use kernel::hil::time;
 use kernel::hil::time::ConvertTicks as _;
 
 use capsules;
 
-#[derive(Debug)]
-struct Component {
-    command_no: usize,
-    args: (usize, usize),
-    t_start: usize,
-}
-
-/// Peripheral usage by time.
-#[derive(Debug)]
-struct Usage {
-    command_no: usize,
-    args: (usize, usize),
-    start: usize,
-}
-
 pub struct SimultaneousAccounting<A: 'static + time::Frequency,
                                   B: 'static + time::Ticks>
 {
     time_source: &'static dyn time::Time<Frequency = A, Ticks = B>,
+    accounted_energy: Cell<usize>,
+    last_update_us: Cell<u32>,
+    adc_state: Cell<u8>,
 }
 
 impl<A: 'static + time::Frequency,
@@ -35,6 +25,9 @@ impl<A: 'static + time::Frequency,
     pub fn new(time_source: &'static dyn time::Time<Frequency = A, Ticks = B>) -> SimultaneousAccounting<A, B> {
         SimultaneousAccounting {
             time_source,
+            accounted_energy: Cell::new(0),
+            last_update_us: Cell::new(0),
+            adc_state: Cell::new(0b0000_0000),
         }
     }
 }
@@ -42,8 +35,13 @@ impl<A: 'static + time::Frequency,
 impl<A: 'static + time::Frequency,
      B: 'static + time::Ticks>
     DriverEnergyAccounting for SimultaneousAccounting<A, B> {
-    fn update(&self, driver_no: usize, command_no: usize, arg0: usize, arg1: usize) {
-        let t_call = self.time_source.ticks_to_us(self.time_source.now());
+        fn update(&self, driver_no: usize, command_no: usize, arg0: usize, arg1: usize) {
+            let t_call = self.time_source.ticks_to_us(self.time_source.now());
+
+            // Update accounted usage for the time since the previous update call.
+            let d_prev_call = t_call - self.last_update_us.get();
+            // TODO: update data here.
+            self.last_update_us.set(t_call);
 
         match driver_no {
             capsules::channeled_adc::DRIVER_NUM => {
