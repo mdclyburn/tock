641e35555388df4ce18904fc6a8cb7d8b9cb5140
Marshall Clyburn <mardev2013@gmail.com>
2026-05-29 12:51:06 -0400

    Documentation for role module

--- capsules/system/src/ipc/userspace_services/role/mod.rs
+++ capsules/system/src/ipc/userspace_services/role/mod.rs
@@ -1,8 +1,15 @@
-/*! Service roles.
+/*! Userspace service service interfaces.
+
+Service interfaces bridge a HIL client to the userspace service application through the registry,
+preventing the HIL client from needing code to interact with the registry.
  */
 
 pub mod digest;
 
+/// Service function identifier.
+///
+/// Identifies the userspace service by its function.
+/// The Role identifies the kind of userspace service to address in a usercall.
 #[derive(Debug, PartialEq)]
 pub enum Role {
     Digest               = 0x11,
